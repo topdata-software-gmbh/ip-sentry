951cd139de38e917a9d452411a3bd5cbba0f80e9
Marc Christenfeldt (Desktop) <marc.christenfeldt@gmx.de>
2026-04-06 13:06:56 +0200

    chore: Add GeoIP City database support and data directory ignore
    - Add .repomixignore to exclude /data directory
    - Update GeoIP database fetch script to use wget instead of curl
    - Add support for downloading GeoLite2-City database

--- scripts/fetch-geoip-database-v2.sh
+++ scripts/fetch-geoip-database-v2.sh
@@ -1,4 +1,7 @@
 #/bin/bash
 #
 # check https://github.com/wp-statistics/GeoLite2-Country
-curl https://cdn.jsdelivr.net/npm/geolite2-country/GeoLite2-Country.mmdb.gz
+wget https://cdn.jsdelivr.net/npm/geolite2-country/GeoLite2-Country.mmdb.gz
+
+# check https://github.com/wp-statistics/GeoLite2-City
+https://cdn.jsdelivr.net/npm/geolite2-city/GeoLite2-City.mmdb.gz
