276ab64ddea812831826cc4ea92a157f8a3220f5
Marc Christenfeldt (Desktop) <marc.christenfeldt@gmx.de>
2026-04-06 17:29:40 +0200

    feat: Replace IP2Location with GeoLite2 GeoIP database

--- scripts/fetch-geoip-database-v2.sh
+++ scripts/fetch-geoip-database-v2.sh
@@ -1,7 +1,8 @@
 #/bin/bash
-#
+
 # check https://github.com/wp-statistics/GeoLite2-Country
 wget https://cdn.jsdelivr.net/npm/geolite2-country/GeoLite2-Country.mmdb.gz
 
-# check https://github.com/wp-statistics/GeoLite2-City
-https://cdn.jsdelivr.net/npm/geolite2-city/GeoLite2-City.mmdb.gz
+
+# # check https://github.com/wp-statistics/GeoLite2-City
+# https://cdn.jsdelivr.net/npm/geolite2-city/GeoLite2-City.mmdb.gz
